f04ed2af4ac8f825356a18128f12b21d728fe699
adrian <adrian_mundo@live.com>
2023-04-26 01:43:16 +0000

    Commit

---
+++ helloworld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge - My First Bash Script
+# Author:                       Adrian Mundo
+# Date of latest revision:      20230425
+# Purpose:                      First Bash script
+
+# Declaration of variables
+
+messagevar="I am working perfectly"
+
+# Declaration of functions
+
+
+# Main
+
+echo $messagevar
+echo 
+
+# End
