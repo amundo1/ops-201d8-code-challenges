260bdb8a88c78005a52ea41188bcd79bc1c82ee8
adrian <adrian_mundo@live.com>
2023-04-26 17:29:49 +0000

    Commit

---
+++ ops201d8_day2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge - Day2 demo Bash Script
+# Author:                       Adrian Mundo
+# Date of latest revision:      20230426
+# Purpose:                      Demo Bash script
+
+# Declaration of variables
+
+
+
+# Declaration of functions
+
+
+# Main
+
+
+# End
