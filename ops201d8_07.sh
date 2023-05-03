c8b2e104da921c6adfd093dca6a2014f3fc01752
adrian <adrian_mundo@live.com>
2023-05-03 17:09:42 +0000

    commit

--- ops201d8_07.sh
+++ ops201d8_07.sh
@@ -1,31 +1,48 @@
 #!/bin/bash
 
-# Script Name:                  Ops Challenge - Day2 demo Bash Script
+# Script Name:                  Ops Challenge - Day 07
 # Author:                       Adrian Mundo
-# Date of latest revision:      20230426
-# Purpose:                      Demo Bash script
+# Date of latest revision:      20230502
+# Purpose:                      Uses grep to remove irrelevant information from the lshw output
+#Add text to the output clearly indicating which component (such as CPU, RAM, etc.) the script is returning information about
 
 # Declaration of variables
-loginHist=last
-messageVar="This is the login history."
-i=0
-
+#computerName= head -n 1 $lookUpFile
+cpu=("*-cpu" 6)
+ram=("*-memory" 3)
+displayAdapter=("*-display" 5)
+netAdapter=("*-network" 14)
+lookUpFile="lookuplshw.txt"
 
 # Declaration of functions
-loginHistOutput () {
-    $loginHist
-    echo
-    echo -e $messageVar "\n\n"
-    echo -e "**********************************************************************\n"
-    echo -e $1 '\n\n'
-    echo -e "***********************************************************************\n"
+greplshw () { 
+
+echo -e "\nComputer Name: \n"
+head -n 1 $lookUpFile
+
+
+echo -e "\nCPU info: \n"    
+grep -A ${cpu[1]}  ${cpu[0]} $lookUpFile | grep -vw "version"
+
+echo -e "\nRAM info: \n"
+grep -A ${ram[1]}  ${ram[0]} $lookUpFile
+
+echo -e "\nDisplay Adapter info: \n"
+grep -A ${displayAdapter[1]}  ${displayAdapter[0]} $lookUpFile
+
+echo -e "\nNetwork Adapter info: \n"
+grep -A ${netAdapter[1]}  ${netAdapter[0]} $lookUpFile
 
 
 }
 
 # Main
-loginHistOutput 1
-loginHistOutput 2
-loginHistOutput 3
+echo -e "\n\n\n*****************************************************Starting lshw Look up***********************************************\n\n\n"
 
+sleep 2
+
+sudo lshw > $lookUpFile
+greplshw
+
+echo -e "\n\n\n***************************************************** lshw Look up Complete***********************************************\n\n\n"
 # End
