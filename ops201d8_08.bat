0a57a15e253695526cb6afaa0728af67fcae3a68
adrian <adrian_mundo@live.com>
2023-05-04 01:34:23 +0000

    commit

---
+++ ops201d8_08.bat
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge - Day 08
+# Author:                       Adrian Mundo
+# Date of latest revision:      20230503
+# Purpose:                      Create a batch file that recursively copies Jorge’s work files on his desktop (recursively meaning to include folder contents).
+#Your script should use ROBOCOPY as the primary means of copying the data.
+
+# Declaration of variables
+
+
+# Declaration of functions
+
+
+# Main
+ROBOCOPY "c:\users\j.thompson\desktop\my project files" "e:\my project files" /e
+# End
