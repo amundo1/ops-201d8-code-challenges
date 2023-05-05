09898a2f1bba3b87f472a09d3b9272a7156f8550
Adrian Mundo <129327614+amundo1@users.noreply.github.com>
2023-05-04 19:58:46 -0700

    Create Ops201d8_09.ps1

---
+++ Ops201d8_09.ps1
@@ -0,0 +1,13 @@
+<# Powershell
+
+# Script Name:                  Ops Challenge - Day 09
+# Author:                       Adrian Mundo
+# Date of latest revision:      20230504
+#Purpose: Create a Powershell script that performs these operations on separate lines. The overall script is not designed to operate holistically, but rather act as a reference for how to execute various interesting operations with the process family of Powershell commandlets. Clearly indicate with comments each component below.
+
+Output all events from the System event log that occurred in the last 24 hours to a file on your desktop named last_24.txt.
+Output all “error” type events from the System event log to a file on your desktop named errors.txt.
+Print to the screen all events with ID of 16 from the System event log.
+Print to the screen the most recent 20 entries from the System event log.’
+Print to the screen all sources of the 500 most recent entries in the System event log. Ensure that the full lines are displayed (get rid of the … and show the entire text).
+#>
