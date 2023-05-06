32077c4cc16669790bd4e8c58ce0ccb1c203b1e3
adrian <adrian_mundo@live.com>
2023-05-06 01:38:37 +0000

    commit

--- ops201d8_10.ps1
+++ ops201d8_10.ps1
@@ -4,40 +4,74 @@
 # Author:                       Adrian Mundo
 # Date of latest revision:      20230504
 #Purpose: Create a Powershell script that performs these operations on separate lines. The overall script is not designed to operate holistically, but rather act as a reference for how to execute various interesting operations with the process family of Powershell commandlets. Clearly indicate with comments each component below.
-Output all events from the System event log that occurred in the last 24 hours to a file on your desktop named last_24.txt.
-Output all “error” type events from the System event log to a file on your desktop named errors.txt.
-Print to the screen all events with ID of 16 from the System event log.
-Print to the screen the most recent 20 entries from the System event log.’
-Print to the screen all sources of the 500 most recent entries in the System event log. Ensure that the full lines are displayed (get rid of the … and show the entire text).
+Print to the terminal screen all active processes ordered by highest CPU time consumption at the top.
+Print to the terminal screen all active processes ordered by highest Process Identification Number at the top.
+Print to the terminal screen the top five active processes ordered by highest Working Set (WS(K)) at the top.
+Start a browser process (such as Google Chrome or MS Edge) and have it open https://owasp.org/www-project-top-ten/.
+Start the process Notepad ten times using a for loop.
+Close all instances of the Notepad.
+Kill a process by its Process Identification Number. Choose a process whose termination won’t destabilize the system, such as Google Chrome or MS Edge.
 #>
 # Declaration of variables
-$desktopPath = [Environment]::GetFolderPath("Desktop")
-$oneDayLog = Get-EventLog -Logname System -After (GET-Date).AddDays(-1)
-$sysErrorLog =  Get-EventLog -Logname System -EntryType Error
-$printEventID16 = Get-EventLog -Logname System -InstanceId 16
-$top20Events = Get-EventLog -Logname System -Newest 20
-$eventSources500 = Get-EventLog -Logname System -Newest 500 | Group-Object -Property Source -NoElement 
+#$desktopPath = [Environment]::GetFolderPath("Desktop")
+$i = 0
+$procid=""
+$startProc = "notepad.exe"
+$stopProc = "notepad"
+$cpuTimeProc = Get-Process | Sort-Object CPU -Descending
+$procIDProc =  Get-Process | Sort-Object Id -Descending
+$wkSetTop5Proc = Get-Process | Sort-Object WorkingSet -Descending | Select-Object -First 5 
+$procIDbyName = Get-Process
+
 
 # Declaration of functions
+function opennotepad10times {
+    for ($i=0; $i -lt 10; $i++)
+{
+    Start-Process $startProc
+}
+}
 
 
 
 # Main
-Write-Output "`n`nPrinting System events in last 24 Hours to last_24.txt `n" 
-Out-File -FilePath $desktopPath\last_24.txt -InputObject $oneDayLog
+Write-Output "`n`n`n*******************************Processes Ordered by CPU time Highest to Lowest*****************************`n`n"
+$cpuTimeProc
+Write-Output "`n`n*******************************Done*****************************`n`n"
 
-Write-Output "Printing System errors to errors.txt `n" 
-Out-File -FilePath $desktopPath\error.txt -InputObject $sysErrorLog
+Write-Output "`n`n`n*******************************Processes Ordered by Process ID; Highest to Lowest*****************************`n`n"
+$procIDProc
+Write-Output "`n`n*******************************Done*****************************`n`n"
 
-Write-Output "Printing System events with Event ID 16 `n" 
-$printEventID16
+Write-Output "`n`n`n*******************************Processes Ordered by Top 5 Working Set; Highest to Lowest*****************************`n`n"
+$wkSetTop5Proc
+Write-Output "`n`n*******************************Done*****************************`n`n"
 
-Write-Output "`n`n"
+Write-Output "`n`n`n*******************************Start Google Chrome and Navigate to  https://owasp.org/www-project-top-ten/ *****************************`n`n"
+[system.Diagnostics.Process]::Start("chrome","https://owasp.org/www-project-top-ten/")
 
-Write-Output "Printing Latest 20 System events `n" 
-$top20Events
+Write-Output "`n`n`n*******************************Start Notepad*****************************`n`n"
+opennotepad10times
 
-Write-Output "Printing Latest 500 System events Sources `n" 
-$eventSources500
+Start-Sleep -Seconds 5
+
+Write-Output "`n`n`n*******************************Stop Notepad*****************************`n`n"
+Stop-Process -Name $stopProc -Force
+
+Start-Sleep -Seconds 5
+
+Write-Output "`n`n`n*******************************Kill Process by PID*****************************`n`n"
+Start-Process $startProc
+
+Start-Sleep -Seconds 5
+$procIDbyName
+Start-Sleep -Seconds 5
+
+$procid = Read-Host "`n`nType the PID to Kill: "
+$procid = [int]$procid
+Stop-Process -id $procid -Force
+
+
+
 
 # End
