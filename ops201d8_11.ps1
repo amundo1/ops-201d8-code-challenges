56066ee9cc25a889a0ce153f1c98b4972c009f7f
Adrian Mundo <129327614+amundo1@users.noreply.github.com>
2023-05-08 20:13:53 -0700

    Create ops201d8_11.ps1

---
+++ ops201d8_11.ps1
@@ -0,0 +1,22 @@
+<# Powershell
+
+# Script Name:                  Ops Challenge - Day 11
+# Author:                       Adrian Mundo
+# Date of latest revision:      20230508
+#Purpose: Write a Powershell script that automates the configuration of a new Windows 10 endpoint. Your script should perform the following:
+
+Enable File and Printer Sharing
+Allow ICMP traffic
+Enable Remote management
+Remove bloatware
+Enable Hyper-V
+Disable SMBv1, an insecure protocol
+#>
+
+# Define Variables
+
+# Define Functions
+
+# MAIN
+
+#END
