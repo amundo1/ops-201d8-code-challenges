3de32bf5a225bfb579e9dc0f23384680df8b4fb6
adrian <adrian_mundo@live.com>
2023-05-10 22:47:04 +0000

    commit

---
+++ ops201d8_13.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge - Day 13
+# Author:                       Adrian Mundo
+# Date of latest revision:      20230510
+# Purpose:                      Create a script that asks a user to type a domain, then displays information about the typed domain. Operations that must be used include:
+#whois
+#dig
+#host
+#nslookup
+#
+# Declaration of variables
+
+
+
+# Declaration of functions
+
+
+# Main
+
+
+
+
+
+
+
+# End
